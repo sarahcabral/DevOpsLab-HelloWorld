c6d40031ea803414fcb193b08c46cb1b06baf19a
sarah.ly2009 <sarah.ly2009@gmail.com>
2022-05-10 11:02:44 -0400

    Teste com erro

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Sarah Cabral")
+        self.assertRegex(result.data.decode(), "Pipeline não DevOps - Sarah Cabral")
 
 
 if __name__ == "__main__":
